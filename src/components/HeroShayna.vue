696e05986fe2efa8be0b9deec749372f7c836add
denissnation <64584104+denissnation@users.noreply.github.com>
2020-09-03 15:29:25 +0700

    Update HeroShayna.vue

--- src/components/HeroShayna.vue
+++ src/components/HeroShayna.vue
@@ -8,7 +8,7 @@
             <div class="row">
               <div class="col-lg-5">
                 <span>Bag,kids</span>
-                <h1>Black Friday</h1>
+                <h1>SendGrid</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
                 <a href="#" class="primary-btn">Shop Now</a>
               </div>
